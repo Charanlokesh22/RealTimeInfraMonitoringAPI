6830232aa0776a06e3aceea1c5c85ff3c41b6f05
Charanlokesh22 <charanlokesh2918@gmail.com>
2025-08-08 15:04:03 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class Event(BaseModel):
+    agent_id: str
+    cpu_usage: float
+    memory_usage: float
+    response_time: float
+
+class User(BaseModel):
+    username: str
+    password: str
